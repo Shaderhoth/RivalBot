668ba15f2926c39dbf37dd95c1441ea8c4cee3a1
BuildTools <davidkuc6@gmail.com>
2018-09-29 19:35:46 +0100

    1

--- Music.py
+++ Music.py
@@ -1,7 +1,23 @@
 import asyncio
 import discord
 from discord.ext import commands
-import opus
+from discord import opus
+
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+        try:
+            opus.load_opus(opus_lib)
+            return
+        except OSError:
+            pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
 
 def __init__(self, bot):
         self.bot = bot
